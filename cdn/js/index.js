8526740c47a62357281ef01b3d8c10d491c68db6
Kaan Ucar <kaanucr2@gmail.com>
2023-11-04 01:53:41 +0300

    index sayfası düzenlendi.

--- cdn/js/index.js
+++ cdn/js/index.js
@@ -1,66 +1,65 @@
-var entrySliderThumbs = new Swiper(".entry-slider-thumbs", {
-    centeredSlidesBounds: true,
-    slidesPerView: 2,
-    watchOverflow: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    direction: 'vertical',
-    autoHeight: true,
-    breakpoints: {
-        // when window width is >= 320px
-        320: {
-            slidesPerView: 2
-        },
-        // when window width is >= 480px
-        480: {
-            slidesPerView: 5
-        },
-        // when window width is >= 640px
-        1600: {
-            slidesPerView: 6
+const peraleadHome = {
+    entrySliderThumbs: new Swiper(".entry-slider-thumbs", {
+        centeredSlidesBounds: true,
+        slidesPerView: 2,
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        direction: 'vertical',
+        autoHeight: true,
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 2
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 5
+            },
+            // when window width is >= 640px
+            1600: {
+                slidesPerView: 6
+            }
         }
+    }),
+    entrySlider: new Swiper(".entry-slider", {
+        autoplay: {
+            delay: 5000,
+        },
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        preventInteractionOnTransition: true,
+        navigation: false,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
+        thumbs: {
+            swiper: this.entrySliderThumbs
+        }
+    }),
+    sidebar: new StickySidebar('#sidebar-wrapper', {
+        containerSelector: '#blog-list-section',
+        innerWrapperSelector: '#blog-sidebar',
+        topSpacing: document.querySelector("#header").offsetHeight + 20,
+        bottomSpacing: 20
+    }),
+    sticky: () => {
+        (window.innerWidth > 1199) ? peraleadHome.sidebar.updateSticky() : peraleadHome.sidebar.destroy();
     }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    peraleadHome.entrySlider.on('slideChangeTransitionStart', function () {
+        entrySliderThumbs.slideTo(entrySlider.activeIndex);
+    });
+    peraleadHome.entrySliderThumbs.on('transitionStart', function () {
+        entrySlider.slideTo(entrySliderThumbs.activeIndex);
+    });
+
+    peraleadHome.sticky();
+    window.addEventListener("resize", function () {
+        peraleadHome.sticky();
+    })
 });
-
-var entrySlider = new Swiper(".entry-slider", {
-    autoplay: {
-        delay: 5000,
-    },
-    watchOverflow: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-    preventInteractionOnTransition: true,
-    navigation: false,
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    thumbs: {
-        swiper: entrySliderThumbs
-    }
-});
-
-entrySlider.on('slideChangeTransitionStart', function () {
-    entrySliderThumbs.slideTo(entrySlider.activeIndex);
-});
-
-entrySliderThumbs.on('transitionStart', function () {
-    entrySlider.slideTo(entrySliderThumbs.activeIndex);
-});
-
-var sidebar = new StickySidebar('#sidebar-wrapper', {
-    containerSelector: '#blog-list-section',
-    innerWrapperSelector: '#blog-sidebar',
-    topSpacing: document.querySelector("#header").offsetHeight + 20,
-    bottomSpacing: 20
-});
-
-sticky();
-
-function sticky() {
-    (window.innerWidth > 1199) ? sidebar.updateSticky() : sidebar.destroy();
-}
-
-window.addEventListener("resize", function () {
-    sticky();
-})
